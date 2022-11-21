432d0419ae961340672445dec5d78ab98a132c85
uzlopak <aras.abbasi@googlemail.com>
2022-11-21 20:22:17 +0100

    nodenext compatiblity

--- types/index.d.ts
+++ types/index.d.ts
@@ -26,73 +26,79 @@ declare module 'fastify' {
   }
 }
 
-export const fastifySwaggerUi: FastifyPluginCallback<FastifySwaggerUiOptions>;
+type FastifySwaggerUi = FastifyPluginCallback<fastifySwaggerUi.FastifySwaggerUiOptions>;
 
-export interface FastifySwaggerUiOptions {
-  baseDir?: string;
-  /**
-   * Overwrite the swagger url end-point
-   * @default /documentation
-   */
-  routePrefix?: string;
-  /**
-   * Swagger UI Config
-   */
-  uiConfig?: FastifySwaggerUiConfigOptions
-  initOAuth?: FastifySwaggerInitOAuthOptions
-  /**
-   * CSP Config
-   */
-  staticCSP?: boolean | string | Record<string, string | string[]>
-  transformStaticCSP?: (header: string) => string
-  /**
-   * route hooks
-   */
-  uiHooks?: FastifySwaggerUiHooksOptions
+declare namespace fastifySwaggerUi {
+  export interface FastifySwaggerUiOptions {
+    baseDir?: string;
+    /**
+     * Overwrite the swagger url end-point
+     * @default /documentation
+     */
+    routePrefix?: string;
+    /**
+     * Swagger UI Config
+     */
+    uiConfig?: FastifySwaggerUiConfigOptions
+    initOAuth?: FastifySwaggerInitOAuthOptions
+    /**
+     * CSP Config
+     */
+    staticCSP?: boolean | string | Record<string, string | string[]>
+    transformStaticCSP?: (header: string) => string
+    /**
+     * route hooks
+     */
+    uiHooks?: FastifySwaggerUiHooksOptions
 
-  transformSpecification?: (swaggerObject: Readonly<Record<string, any>>, request: FastifyRequest, reply: FastifyReply) => Record<string, any>
-  transformSpecificationClone?: boolean
+    transformSpecification?: (swaggerObject: Readonly<Record<string, any>>, request: FastifyRequest, reply: FastifyReply) => Record<string, any>
+    transformSpecificationClone?: boolean
+  }
+
+  export type FastifySwaggerUiConfigOptions = Partial<{
+    deepLinking: boolean
+    displayOperationId: boolean
+    defaultModelsExpandDepth: number
+    defaultModelExpandDepth: number
+    defaultModelRendering: string
+    displayRequestDuration: boolean
+    docExpansion: string
+    filter: boolean | string
+    layout: string
+    maxDisplayedTags: number
+    showExtensions: boolean
+    showCommonExtensions: boolean
+    useUnsafeMarkdown: boolean
+    syntaxHighlight: {
+      activate?: boolean
+      theme?: string
+    } | false
+    tryItOutEnabled: boolean
+    validatorUrl: string | null
+    supportedSubmitMethods: Array<'get' | 'post' | 'put' | 'delete' | 'patch' | 'options'>
+    persistAuthorization: boolean
+  }>
+
+  export type FastifySwaggerInitOAuthOptions = Partial<{
+    clientId: string,
+    clientSecret: string,
+    realm: string,
+    appName: string,
+    scopeSeparator: string,
+    scopes: string | string[],
+    additionalQueryStringParams: { [key: string]: any },
+    useBasicAuthenticationWithAccessCodeGrant: boolean,
+    usePkceWithAuthorizationCodeGrant: boolean
+  }>
+
+  export type FastifySwaggerUiHooksOptions = Partial<{
+    onRequest?: onRequestHookHandler,
+    preHandler?: preHandlerHookHandler,
+  }>
+
+  export const fastifySwaggerUi: FastifySwaggerUi
+  export { fastifySwaggerUi as default }
 }
+declare function fastifySwaggerUi(...params: Parameters<FastifySwaggerUi>): ReturnType<FastifySwaggerUi>
 
-export type FastifySwaggerUiConfigOptions = Partial<{
-  deepLinking: boolean
-  displayOperationId: boolean
-  defaultModelsExpandDepth: number
-  defaultModelExpandDepth: number
-  defaultModelRendering: string
-  displayRequestDuration: boolean
-  docExpansion: string
-  filter: boolean | string
-  layout: string
-  maxDisplayedTags: number
-  showExtensions: boolean
-  showCommonExtensions: boolean
-  useUnsafeMarkdown: boolean
-  syntaxHighlight: {
-    activate?: boolean
-    theme?: string
-  } | false
-  tryItOutEnabled: boolean
-  validatorUrl: string | null
-  supportedSubmitMethods: Array<'get' | 'post' | 'put' | 'delete' | 'patch' | 'options'>
-  persistAuthorization: boolean
-}>
-
-export type FastifySwaggerInitOAuthOptions = Partial<{
-  clientId: string,
-  clientSecret: string,
-  realm: string,
-  appName: string,
-  scopeSeparator: string,
-  scopes: string | string[],
-  additionalQueryStringParams: { [key: string]: any },
-  useBasicAuthenticationWithAccessCodeGrant: boolean,
-  usePkceWithAuthorizationCodeGrant: boolean
-}>
-
-export type FastifySwaggerUiHooksOptions = Partial<{
-  onRequest?: onRequestHookHandler,
-  preHandler?: preHandlerHookHandler,
-}>
-
-export default fastifySwaggerUi;
+export = fastifySwaggerUi;
