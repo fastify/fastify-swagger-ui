118d8b68c21ea2b10c281cef9e5857b92b39ad03
Sebastian <marius.sebastian.besel.github@gmail.com>
2023-11-05 16:51:27 +0100

    feat: add e2e tests with playwright

---
+++ examples/example-e2e.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const fastify = require('fastify')({ logger: true })
+const readFileSync = require('node:fs').readFileSync
+const resolve = require('node:path').resolve
+
+const exampleLogo = readFileSync(
+  resolve(__dirname, '..', 'examples/static', 'example-logo.svg'),
+  'utf8'
+)
+
+fastify.register(require('@fastify/swagger'), {
+  mode: 'static',
+  specification: {
+    path: './examples/example-static-specification.json'
+  }
+})
+
+readFileSync(
+  resolve(__dirname, '..', 'static', 'logo.svg'),
+  'utf8'
+)
+
+fastify.register(require('../index'), {
+  theme: {
+    js: [
+      { filename: 'unloaded.js', content: 'window.onbeforeunload = function(){alert("unloaded test-theme")}' }
+    ],
+    css: [
+      { filename: 'theme.css', content: '.download-url-button {background: red !important;}' }
+    ],
+    favicon: [
+      {
+        filename: 'favicon.svg',
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/svg+xml',
+        content: exampleLogo
+      }
+    ]
+  },
+  logo: {
+    type: 'image/svg+xml',
+    content: exampleLogo
+  }
+})
+
+fastify.listen({ port: 3000 }, (err) => {
+  if (err) throw err
+  fastify.log.info('visit the documentation at http://127.0.0.1:3000/documentation/')
+})
---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+const { defineConfig, devices } = require('@playwright/test')
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://127.0.0.1:3000/documentation',
+    trace: 'on-first-retry'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    }
+  ],
+  webServer: {
+    command: 'npm run test:e2e:command',
+    url: 'http://127.0.0.1:3000/documentation',
+    reuseExistingServer: !process.env.CI
+  }
+})
